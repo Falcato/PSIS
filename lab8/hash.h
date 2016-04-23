68a394f045ccef7a91cbe65a0869696067e118dd
Ricardo Miranda <ricardo.miranda704@gmail.com>
2016-04-23 19:37:42 +0100

    lab8, mutexes, bem organizado, a funcionar

---
+++ lab8/hash.h
@@ -0,0 +1,30 @@
+#ifndef hashHeader
+#define hashHeader
+
+
+struct entry_s {
+	int key;
+	char *value;
+	struct entry_s *next;
+};
+
+typedef struct entry_s entry_t;
+
+struct hashtable_s {
+	int size;
+	struct entry_s **table;	
+};
+
+typedef struct hashtable_s hashtable_t;
+
+
+hashtable_t *ht_create( int size );
+int ht_hash( hashtable_t *hashtable, char *key );
+int ht_hash1(hashtable_t *hashtable, int a);
+entry_t *ht_newpair( int key, char *value );
+int ht_set( hashtable_t *hashtable, int key, char *value, int flag );
+char *ht_get( hashtable_t *hashtable, int key );
+char *ht_print( hashtable_t *hashtable, int key );
+void ht_remove(hashtable_t *hashtable, int key);
+
+#endif
---
+++ lab8/no-sync.c
@@ -0,0 +1,69 @@
+#include <limits.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+
+
+#define N_THREADS 4
+#define MAX_VAL UINT_MAX 
+
+unsigned int counter;
+
+void * thread_code(void *arg){
+	unsigned int start;
+	unsigned int  end;
+	unsigned int priv_count = 0;
+	int thread_indx =(int)  arg;
+	if (thread_indx == 0){
+		start = 0;
+	}else{
+		start = MAX_VAL/N_THREADS * thread_indx +1;
+	}
+	if (thread_indx == N_THREADS -1){
+		end = MAX_VAL;
+	}else{
+		end = MAX_VAL/N_THREADS * (thread_indx +1) ;
+	}
+	
+	printf("thread %d \t start (%u) \t end %u \t thread_id %ul\n", thread_indx, start, end, pthread_self());
+
+	for (unsigned int  i = start; i <  end; i++){
+//		counter ++;
+		priv_count ++;
+	}
+	counter += priv_count;
+	printf("thread end %ul %u\n", pthread_self(), priv_count);
+}
+
+void error_and_die(const char *msg) {
+  perror(msg);
+  exit(EXIT_FAILURE);
+}
+
+
+int main(){
+	counter = 0;
+	pthread_t thread_id[20];
+		
+	
+	for (int i = 0 ; i < N_THREADS; i++){
+		pthread_create(&thread_id[i], 
+						NULL, 
+						thread_code, 
+						(void *) i);
+	}
+
+
+	for (int i = 0; i < N_THREADS; i++){
+		pthread_join(thread_id[i], NULL);
+	}
+	printf("resultado %u\n", counter);
+}
