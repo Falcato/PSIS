5f0b6217abf825b3a1a9bbe04d76acf5d27d56b8
Ricardo Miranda <ricardo.miranda704@gmail.com>
2016-05-16 13:55:42 +0100

    Continuação do "proj with restart" ja com fix

---
+++ Proj2/front.c
@@ -0,0 +1,213 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <signal.h>
+#define DATA_PORT 9000
+
+//the thread functions
+void *connection_handler();
+void *keyboard_handler();
+void *FIFO_handler();
+
+//the thread ids
+pthread_t thread_id;
+pthread_t thread_id2;
+pthread_t thread_id3;
+
+//signal global variable
+volatile sig_atomic_t stop;
+
+//FIFO FILES
+char fifo_name1[] = "/tmp/fifo1";
+char fifo_name2[] = "/tmp/fifo2";
+
+//shutdown signal handler
+void inthand(int signum) {
+	printf("\nRECEIVED SHIT DOWN SIGNAL\n");
+	int fd1;	
+	char msg [32];
+	/* write to the FIFO */
+	fd1 = open(fifo_name1, O_RDWR);
+	strcpy(msg,"quit");
+	write(fd1, msg, strlen(msg)+1);
+	//close(fd1);
+	
+    exit(2);
+}
+
+// This will handle connection for each client
+void *connection_handler(){
+	
+	int fd, newfd, addrlen;
+	struct sockaddr_in addr;
+	
+	//Create socket
+	if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) exit(1);
+	puts("Socket created");
+	
+	//Prepare the sockaddr_in structure
+	memset((void*)&addr, (int)'\0', sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(9999);
+	
+	//Bind
+	if(bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) exit(1);
+	puts("Bind done");
+	
+	//Listen
+	if(listen(fd,5) == -1)
+	exit(1);//error
+	
+	
+	//RECIEVES SHUTDOWN SIGNAL 
+	signal(SIGINT, inthand);
+	
+	addrlen = sizeof(addr);    
+    
+    
+    while( (newfd = accept(fd, (struct sockaddr*)&addr, &addrlen)) ){
+		
+		if(-1 == write(newfd, "9000", 4)){
+			printf("Erro: \n");
+		}
+	}
+	
+}
+
+// This will handle the keyboard inputs
+void *keyboard_handler(){
+	char buf2[32];
+	char msg[32];
+	bzero(buf2, 32);
+    //OPEN FIFOS TO READ AND WRITE
+    int s2c = open(fifo_name1, O_RDWR);
+    
+	//RECIEVES SHUTDOWN SIGNAL 
+	signal(SIGINT, inthand);
+	
+	while (1){
+		
+		if(read(0, buf2, 32) > 0){
+			if(strcmp(buf2,"quit\n") == 0){
+				
+				strcpy(msg,"quit");
+				write(s2c, msg, strlen(msg)+1);
+				
+				exit(0);
+			}
+        }
+		bzero(buf2, 32);
+	}
+}
+
+void *FIFO_handler(){
+    int c2s, s2c, c=0, i;
+	char msg[80], buf[10];
+    pid_t id;
+    struct stat st;
+        // CREATE FIFOS IF THEY DONT EXIST
+    if (stat(fifo_name1, &st) != 0)
+        mkfifo(fifo_name1, 0666);
+    if (stat(fifo_name2, &st) != 0)
+        mkfifo(fifo_name2, 0666);
+        
+    //OPEN FIFOS TO READ AND WRITE
+    s2c= open(fifo_name1, O_RDWR);
+    c2s= open(fifo_name2, O_RDWR);
+    //NON-BLOCK READ FROM FIFO
+	fcntl(s2c, F_SETFL, O_NONBLOCK);
+	fcntl(c2s, F_SETFL, O_NONBLOCK);
+	
+	//COUNTER TO CHECK FOR NON RESPONSIVE PROGRAM
+	c = 0;	
+	
+    //SENDING FIRST MESSAGE
+	strcpy(msg,"front");
+	write(s2c, msg, strlen(msg)+1);
+    while (1){
+
+		//READ FROM THE FIFO
+		i = read(c2s, buf, 10);
+		
+		if ( i > 0){
+			//printf("1 received: %s \n", buf);
+			c=0;
+			//WRITE TO THE FIFO
+			strcpy(msg,"front");
+			write(s2c, msg, strlen(msg)+1);
+		}
+		
+		sleep(1);
+		c++;    
+		if (c>6){
+			//WAITS 5SEC THEN REBOOTS THE SERVER
+			id = fork();
+			if (id == -1){
+				perror("Fork failed");
+			}else if (id > 0){
+				//FATHER
+				c = 0;
+				sleep(3);
+				
+				//SENDING FIRST MESSAGE
+				strcpy(msg,"front");
+				write(s2c, msg, strlen(msg)+1);
+
+			}else if(id == 0){
+				//CHILD
+				char *envp[] = { NULL };
+				char *argv[] = { NULL };
+				execve("./server", argv, envp );
+			}
+		}	
+	}
+}
+
+int main(){
+	
+	//RECIEVES SHUTDOWN SIGNAL 
+	signal(SIGINT, inthand);
+	
+	if(pthread_create(&thread_id, NULL, connection_handler, NULL) < 0 ){
+        perror("could not create thread");
+        exit (1);
+    }
+	if(pthread_create(&thread_id2, NULL , keyboard_handler, NULL) < 0 ){
+        perror("could not create thread");
+        exit (1);
+    }        
+ 	if(pthread_create(&thread_id3, NULL , FIFO_handler, NULL) < 0 ){
+        perror("could not create thread");
+        exit (1);
+    }
+	pthread_join(thread_id, NULL);
+	pthread_join(thread_id2, NULL);
+	pthread_join(thread_id3, NULL);
+	
+	exit(0);
+}
+/*
+TODO
+
+VERIFICAR NOVAMENTE OS LOCKS DE ACORDO COM A PROCURA ASSOCIADA A UMA ESCRITA OU DELETE->done confirmar que e correcto
+FAZER O ALGORITMO DE DETECÇAO DE SAIDA, FRONT ESCREVE IMPAR E DATA ESCREVE PARES NO FIFO->check
+FAZERF CTRL+C NO FRONT SERVER PARA ENVIAR MENSAGEM DE SAIDA PARA A FIFO->check
+FAZERF CTRL+C NO DATA SERVER PARA ENVIAR MENSAGEM DE SAIDA PARA A FIFO->acho que nao e suposto
+FAZER BACKUP QUANDO SAI PELA FIFO (DATA)->check
+OPTIMIZAR BACKUP (A CADA 100 INSTRUÇOES APAGAR LOG E COPIAR A HASH)->check
+ALOCAÇAO E FREES
+
+VERIFICAR O WRITE DO KEYBOARD HANDLER
+*/
---
+++ Proj2/hash.c
@@ -0,0 +1,348 @@
+#define _XOPEN_SOURCE 500 /* Enable certain library functions (strdup) on linux.  See feature_test_macros(7) */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <pthread.h>
+#include "hash.h"
+
+extern pthread_rwlock_t  rwlock;
+extern pthread_rwlock_t  rwlock2;
+
+/* Create a new hashtable. */
+hashtable_t *ht_create( int size ) {
+
+	hashtable_t *hashtable = NULL;
+	int i;
+
+	if( size < 1 ) return NULL;
+
+	/* Allocate the table itself. */
+	if( ( hashtable = malloc( sizeof( hashtable_t ) ) ) == NULL ) {
+		return NULL;
+	}
+
+	/* Allocate pointers to the head nodes. */
+	if( ( hashtable->table = malloc( sizeof( entry_t * ) * size ) ) == NULL ) {
+		return NULL;
+	}
+	for( i = 0; i < size; i++ ) {
+		hashtable->table[i] = NULL;
+	}
+
+	hashtable->size = size;
+
+	return hashtable;	
+}
+
+/* Hash a string for a particular hash table. */
+int ht_hash( hashtable_t *hashtable, char *key ) {
+
+	unsigned long int hashval;
+	int i = 0;
+
+	/* Convert our string to an integer */
+	while( hashval < ULONG_MAX && i < strlen( key ) ) {
+		hashval = hashval << 8;
+		hashval += key[ i ];
+		i++;
+	}
+
+	return hashval % hashtable->size;
+}
+
+
+uint32_t ht_hash1(hashtable_t *hashtable, uint32_t a){
+    /*a = (a ^ 61) ^ (a >> 16);
+    a = a + (a << 3);
+    a = a ^ (a >> 4);
+    a = a * 0x27d4eb2d;
+    a = a ^ (a >> 15);*/
+    return a % hashtable->size;
+}
+
+
+//Create a pair
+entry_t *ht_newpair( uint32_t key, char *value ) {
+	entry_t *newpair;
+
+	if( ( newpair = malloc( sizeof( entry_t ) ) ) == NULL ) {
+		return NULL;
+	}
+
+	newpair->key = key;
+		
+	//if( ( newpair->value = value  ) == NULL ) {
+	if( ( newpair->value = strdup(value)  ) == NULL ) {
+		return NULL;
+	}
+
+	newpair->next = NULL;
+
+	return newpair;
+}
+
+//Insert a pair into a hash table
+int ht_set( hashtable_t *hashtable, uint32_t key, char *value, int flag) {
+	int bin = 0;
+	entry_t *newpair = NULL;
+	entry_t *next = NULL;
+	entry_t *last = NULL;
+
+	//bin = ht_hash( hashtable, key );
+	bin = ht_hash1( hashtable, key );
+
+	next = hashtable->table[ bin ];
+	
+	//READ LOCK1
+	pthread_rwlock_rdlock(&rwlock);
+	
+	while( next != NULL && key != next->key ) {
+		last = next;
+		next = next->next;
+	}
+	
+	
+	/* There's already a pair.  Let's replace that string. */
+	if( next != NULL && key == next->key ) {
+		
+		if(flag == 1){
+			
+			//WRITE LOCK
+			pthread_rwlock_wrlock(&rwlock2);
+				
+			free( next->value );
+			next->value = strdup( value );
+			
+			//WRITE UNLOCK
+			pthread_rwlock_unlock(&rwlock2);
+			
+			return 2;
+		}else{
+			return -2;
+		}
+
+	/* Nope, could't find it.  Time to grow a pair. */
+	}else{
+		newpair = ht_newpair( key, value );
+
+		/* We're at the start of the linked list in this bin. */
+		if( next == hashtable->table[ bin ] ) {
+			
+			//WRITE LOCK
+			//pthread_rwlock_wrlock(&rwlock2);
+			
+			newpair->next = next;
+			hashtable->table[ bin ] = newpair;
+			
+			//WRITE UNLOCK
+			//pthread_rwlock_unlock(&rwlock2);
+			
+			//READ UNLOCK1
+			pthread_rwlock_unlock(&rwlock);
+				
+			return 0;
+		/* We're at the end of the linked list in this bin. */
+		}else if( next == NULL ){
+			//WRITE LOCK
+			//pthread_rwlock_wrlock(&rwlock2);
+
+			last->next = newpair;
+			newpair->next = NULL;
+			
+			//WRITE UNLOCK
+			//pthread_rwlock_unlock(&rwlock2);
+			
+			//READ UNLOCK1
+			pthread_rwlock_unlock(&rwlock);
+			
+			return 0;
+		/* We're in the middle of the list. */
+		}else{
+			//WRITE LOCK
+			//pthread_rwlock_wrlock(&rwlock2);
+			
+			newpair->next = next;
+			last->next = newpair;
+			
+			//WRITE UNLOCK
+			//pthread_rwlock_unlock(&rwlock2);
+			
+			//READ UNLOCK1
+			pthread_rwlock_unlock(&rwlock);
+			return 0;
+		}
+	}
+	return 0;
+}
+
+//Retrieve a pair from a hash table
+char *ht_get( hashtable_t *hashtable, uint32_t key ) {
+	uint32_t bin = 0;
+	entry_t *pair;
+
+	//bin = ht_hash( hashtable, key );
+	bin = ht_hash1( hashtable, key );
+	
+	/* Step through the bin, looking for our value. */
+	pair = hashtable->table[ bin ];
+	printf("Position:%d", bin);
+	
+	//READ LOCK1
+	pthread_rwlock_rdlock(&rwlock);
+	
+	while( pair != NULL && key != pair->key ) {
+		printf(" ------------------->[%d,%s]\n", pair->key, pair->value);
+		pair = pair->next;
+	}
+
+
+	//READ LOCK2
+	pthread_rwlock_rdlock(&rwlock2);
+	
+	//READ UNLOCK1
+	pthread_rwlock_unlock(&rwlock);
+
+	/* Did we actually find anything? */
+	if( pair == NULL || key != pair->key ) {
+		//READ UNLOCK2
+		pthread_rwlock_unlock(&rwlock2);
+		return NULL;
+	}else{
+		printf(" ->[%d,%s]\n", pair->key, pair->value);
+		
+		//READ UNLOCK2
+		pthread_rwlock_unlock(&rwlock2);
+		return pair->value;
+	}
+	
+}
+
+//print the hash
+void ht_print( hashtable_t *hashtable) {
+	uint32_t bin = 0;
+	entry_t *pair;	
+	
+	for(bin = 0; bin < hashtable->size; bin++){
+		
+		/* Step through the bin, looking for our value. */
+		pair = hashtable->table[ bin ];
+		if(pair != NULL){
+			printf("\n");
+			printf("Position:%u", bin);
+		}
+		while( pair != NULL ) {
+			
+			printf(" ->[%d,%s]", pair->key, pair->value);
+			pair = pair->next;
+		}
+	}
+	printf("\n");
+}
+
+//save the hash to a file
+void ht_save( hashtable_t *hashtable, FILE *f ) {
+	uint32_t bin = 0;
+	entry_t *pair;
+
+	for(bin = 0; bin < hashtable->size; bin++){
+		
+		/* Step through the bin, looking for our value. */
+		pair = hashtable->table[ bin ];
+		
+		//WRITE LOCK
+		pthread_rwlock_rdlock(&rwlock);//erro no lock, fica encravado se for write lock, perceber porque
+		
+		while( pair != NULL ) {	
+			fprintf(f, "%s %u %s\n", "rd", pair->key, pair->value);
+			pair = pair->next;
+		}
+		
+		//WRITE UNLOCK
+		pthread_rwlock_unlock(&rwlock);
+		
+	}
+}
+
+//read the hash from a file
+void ht_read( hashtable_t *hashtable, FILE *f) {
+	uint32_t key = 0;
+	char *value;
+	char buff[256];
+	char instr[4];
+	
+	while(fscanf(f, "%s" "%u" "%s",instr, &key, buff) != EOF){
+		if(strcmp("rd", instr)){
+			value = buff;
+			ht_set(hashtable, key, value, 0);
+		}else if(strcmp("wr0", instr)){
+			value = buff;
+			ht_set(hashtable, key, value, 0);
+		}else if(strcmp("wr1", instr)){
+			value = buff;
+			ht_set(hashtable, key, value, 1);
+		}else if(strcmp("rm", instr)){
+			ht_remove(hashtable, key);
+		}
+		
+	}
+}
+
+//remove one pair from the hash
+int ht_remove(hashtable_t *hashtable, uint32_t key){
+	uint32_t bin = 0;
+	int flag = -1;
+	entry_t *next;
+	entry_t *last;
+	//entry_t *aux;
+	//entry_t *pair;
+
+
+
+	bin = ht_hash1( hashtable, key );
+
+	next = hashtable->table[ bin ];	
+	last = next;
+
+	//READ LOCK1
+	pthread_rwlock_rdlock(&rwlock);
+	
+	while(key != next->key ) {
+		last = next;
+		next = next->next;
+	}
+
+	//WRITE LOCK2
+	pthread_rwlock_wrlock(&rwlock2);
+	
+	//READ UNLOCK1
+	pthread_rwlock_unlock(&rwlock);
+	
+	
+	if(next->key == key){
+		//ITS THE ONLY ONE IN THAT ENTRY
+		if(last == next && next->next == NULL){
+			free(next->value);
+			free(next->next);
+			free(hashtable->table[bin]);
+			hashtable->table[bin] = NULL;
+			flag = 1;
+			return flag;
+		}
+		
+		last->next = next->next;
+		free(next->value);
+		free(next->next);
+		free(next);
+
+		flag = 1;
+	
+	}
+	
+	//WRITE UNLOCK2
+	pthread_rwlock_unlock(&rwlock2);	
+
+	return flag;
+}
